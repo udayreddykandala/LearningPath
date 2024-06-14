1cb75fa5f5234fd7cb1f65869f187b11bdf0842c
udayreddykandala <136275899+udayreddykandala@users.noreply.github.com>
2024-06-14 15:33:03 +0100

    enumerate function

--- enumarte_function.py
+++ enumarte_function.py
@@ -9,3 +9,11 @@ print(list(enumerated))
 for index,value in enumerate(fruits):
     filename = f"filename{index}.jpeg"
     print(filename)
+
+
+
+'''
+in javascript you should write something like this and its so easy 
+fruits = ["apple", "watermelon", "lemon"]
+fruits.forEach((fruit, index) => console.log(fruit, index))
+'''
